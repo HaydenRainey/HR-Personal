048cd50c44125ed7d58886f226e3ad62ea764958
Hayden Rainey <s513768@nwmissouri.edu>
2018-11-17 11:30:37 -0600

    Add project files.

---
+++ HR-Personal/Models/ManageViewModels/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Personal.Models.ManageViewModels
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
